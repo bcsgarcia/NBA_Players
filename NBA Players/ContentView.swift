23d6fabb1b01c4785e591ab018bc8156c86c88b1
Bruno Garcia <bcsgarcia@outlook.com>
2019-06-27 21:38:17 -0300

    Initial Commit

---
+++ NBA Players/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NBA Players
+//
+//  Created by Bruno Garcia on 27/06/19.
+//  Copyright © 2019 Bruno Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
